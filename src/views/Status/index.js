8963de329d7611fdb6403ef3dfca9cf114ef0915
Lane Seppala <lane.seppala@joyent.com>
2018-05-22 16:49:48 -0600

    Add Validation issue table

--- src/views/Status/index.js
+++ src/views/Status/index.js
@@ -2,7 +2,7 @@ import m from "mithril";
 import { request } from "mithril";
 import { conchApi } from "config";
 
-import {  Spinner, ViewTitleHero } from "../component";
+import { Spinner, ViewTitleHero } from "../component";
 
 import RackProgress from "./RackProgress";
 
@@ -15,10 +15,28 @@ export default () => {
 	let devices;
 	let rackRooms;
 	let rackCount;
-	let validationFailCount;
+	let failingValidations;
+	let validationPlanIdToName;
+	let validationsToShow = 10;
 	let progress = { pass: 0, total: 0 };
 	const progressPercent = () =>
 		progress.total ? progress.pass / progress.total * 100 : 0;
+
+	const validationToRow = validation => {
+		return m(
+			"tr",
+			m("td[width=5%]", m("i.fa.fa-bell")),
+			m("td", validation.device_id),
+			m(
+				"td",
+				m(
+					"span.has-text-grey-light",
+					validationPlanIdToName[validation.validation_plan_id]
+				)
+			),
+			m("td", m("a.button.is-small.is-primary", "Details"))
+		);
+	};
 
 	const statusTiles = {
 		view: () =>
@@ -56,40 +74,116 @@ export default () => {
 					),
 					m(
 						StatusTile,
-						validationFailCount == null
+						failingValidations == null
 							? m(Spinner)
 							: [
-									m("p.title", validationFailCount),
+									m("p.title", failingValidations.length),
 									m("p.subtitle", "Validation Issues"),
 							  ]
 					)
 				),
+				failingValidations &&
+					failingValidations.length > 0 &&
+					validationPlanIdToName &&
+					m(
+						".tile.is-ancestor.has-text-centered",
+						m(
+							StatusTile,
+							m(
+								".card",
+								m(
+									"header.card-header",
+									m(
+										"p.card-header-title",
+										"Device Validation Issues"
+									)
+								),
+								m(
+									".card-table",
+									m(
+										"table.table.is-fullwidth.is-striped",
+										m(
+											"thead",
+											m(
+												"tr",
+												m("td"),
+												m("td", "Device"),
+												m("td", "Validation Plan"),
+												m("td")
+											)
+										),
+										m(
+											"tbody",
+											failingValidations
+												.slice(0, validationsToShow)
+												.map(validationToRow)
+										)
+									)
+								),
+								validationsToShow < failingValidations.length &&
+									m(
+										"footer.card-footer",
+										m(
+											"a.card-footer-item",
+											{
+												onclick: () => {
+													validationsToShow =
+														failingValidations.length;
+												},
+											},
+											"View All"
+										)
+									)
+							)
+						)
+					),
 				m(
 					".tile.is-ancestor.has-text-centered",
 					m(
 						StatusTile,
-						m("div.box", { style: "background-color:rgba(28%, 61%, 91%, 0.4)" },
+						m(
+							"div.box",
+							{
+								style:
+									"background-color:rgba(28%, 61%, 91%, 0.4)",
+							},
 							rackRooms == null
 								? m(Spinner)
 								: [
-										m("p.subtitle", "Rack Validation Status"),
-									m(RackProgress, { rackRooms, group: "status" } )
-								]
+										m(
+											"p.subtitle",
+											"Rack Validation Status"
+										),
+										m(RackProgress, {
+											rackRooms,
+											group: "status",
+										}),
+								  ]
 						)
 					),
 					m(
 						StatusTile,
-						m("div.box", { style: "background-color:rgba(28%, 61%, 91%, 0.4)" },
+						m(
+							"div.box",
+							{
+								style:
+									"background-color:rgba(28%, 61%, 91%, 0.4)",
+							},
 							rackRooms == null
 								? m(Spinner)
 								: [
-
-										m("p.subtitle", "Validation Status by Role"),
-									m(RackProgress, { rackRooms, group: "role" } )
-								]
+										m(
+											"p.subtitle",
+											"Validation Status by Role"
+										),
+										m(RackProgress, {
+											rackRooms,
+											group: "role",
+										}),
+								  ]
 						)
 					)
-				),
+				)
 			),
 	};
 
@@ -135,7 +229,17 @@ export default () => {
 				}/validation_state?status=error,fail`,
 				withCredentials: true,
 			}).then(res => {
-				validationFailCount = res.length;
+				failingValidations = res;
+			});
+			request({
+				method: "GET",
+				url: `${conchApi}/validation_plan`,
+				withCredentials: true,
+			}).then(res => {
+				validationPlanIdToName = res.reduce((acc, validationPlan) => {
+					acc[validationPlan.id] = validationPlan.name;
+					return acc;
+				});
 			});
 		},
 		view({ attrs: { currentWorkspace } }) {
