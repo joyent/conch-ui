2af284883d19be79116e4b5fe75026a268bf1091
Lane Seppala <lane.seppala@joyent.com>
2018-06-07 15:01:50 -0600

    Validation Tab

--- src/views/DatacenterExplorer/DeviceModal.js
+++ src/views/DatacenterExplorer/DeviceModal.js
@@ -1,11 +1,15 @@
 import m from "mithril";
-import stream from "mithril/stream";
 import moment from "moment";
+import sortBy from "lodash/sortBy";
+import groupBy from "lodash/groupBy";
+import countBy from "lodash/countBy";
+import stream from "mithril/stream";
 import { request } from "mithril";
 
 import { conchApi } from "config";
 
 import { Spinner } from "../component/";
+import { ProgressIcon } from "./Progress";
 
 const Tabs = () => {
 	const activeTab = stream();
@@ -103,23 +107,113 @@ const OverviewTab = () => {
 	};
 };
 
+const ValidationRows = () => {
+	let revealDetails = false;
+	const resultsToCountTags = results => {
+		const counts = countBy(results, "status");
+		const statusToColor = status => {
+			switch (status) {
+				case "fail":
+					return "is-warning";
+				case "error":
+					return "is-danger";
+				default:
+					return "is-info";
+			}
+		};
+		return Object.keys(counts).map(status =>
+			m("span.tag", { class: statusToColor(status) }, counts[status])
+		);
+	};
+	return {
+		view: ({ attrs: { validation, results } }) => [
+			m(
+				"tr",
+				{
+					onclick() {
+						revealDetails = !revealDetails;
+					},
+					class: revealDetails && "is-selected",
+					style: "cursor: pointer"
+				},
+				m(
+					"td",
+					resultsToCountTags(results)
+					//m(ProgressIcon, {
+					//progress: "validated"
+					//})
+				),
+				m("td", validation.name),
+				m(
+					"td",
+					validation.description ||
+						m("span.has-text-grey", "No Description")
+				)
+			),
+			revealDetails &&
+				m(
+					"tr",
+					m(
+						"td[colspan=3]",
+						m(
+							".content",
+							m(
+								"table.table.is-narrow.is-marginless.is-fullwidth",
+								m(
+									"thead",
+									m(
+										"tr",
+										m("th"),
+										m("th", "Status"),
+										m("th", "Message"),
+										m("th", "Hint")
+									)
+								),
+								m(
+									"tbody",
+									sortBy(results, "order").map(result =>
+										m(
+											"tr",
+											m("td", result.order + 1),
+											m("td", result.status),
+											m("td", result.message),
+											m(
+												"td",
+												result.hint ||
+													m(
+														"span.has-text-grey",
+														"No Hint"
+													)
+											)
+										)
+									)
+								)
+							)
+						)
+					)
+				)
+		]
+	};
+};
+
 const ValidationTab = () => {
-	const headers = [
-		m("th", "Category"),
-		m("th", "Name"),
-		//m("th", "Status"),
-		m("th.has-text-right", "Message")
-		//m("th", "Hint")
-	];
+	const headers = [m("th", ""), m("th", "Name"), m("th", "Description")];
 	const validationStates = stream();
 	const validations = stream();
-	const validationIdToName = validations.map(vs => {
+
+	// group results by validation
+	const groupedValidationStateResults = validationStates.map(states =>
+		states.map(state => groupBy(state.results, "validation_id"))
+	);
+	const idToValidation = validations.map(vs => {
 		let map = vs.reduce((acc, v) => {
-			acc[v.id] = v.name;
+			acc[v.id] = v;
 			return acc;
 		}, {});
 		return id => map[id];
 	});
+
+	const revealDetails = {};
 
 	return {
 		oninit: ({ attrs: { activeDevice } }) => {
@@ -138,29 +232,23 @@ const ValidationTab = () => {
 			return stream.merge([validationStates, validations])() == null
 				? m(Spinner)
 				: m(
-						"table.table.is-narrow.is-marginless.is-hoverable",
+						"table.table.is-narrow.is-marginless.is-fullwidth",
 						m("thead", m("tr", headers)),
 						m("tfoot", m("tr", headers)),
-						validationStates().map(validationState =>
-							m(
+						groupedValidationStateResults().map(groupedResults => {
+							return m(
 								"tbody",
-								validationState.results.map(r =>
-									m(
-										"tr",
-										m("td", r.category),
-										m(
-											"td",
-											validationIdToName()(
-												r.validation_id
-											)
-										),
-										//m("td", r.status),
-										//m("td", r.hint),
-										m("td.has-text-right", r.message)
-									)
+								sortBy(
+									Object.keys(groupedResults),
+									vid => idToValidation()(vid).name
+								).map(vid =>
+									m(ValidationRows, {
+										results: groupedResults[vid],
+										validation: idToValidation()(vid)
+									})
 								)
-							)
-						)
+							);
+						})
 				  );
 		}
 	};
@@ -209,6 +297,10 @@ export default () => {
 											},
 											{
 												title: "Networking",
+												component: null
+											},
+											{
+												title: "Settings",
 												component: null
 											},
 											{
