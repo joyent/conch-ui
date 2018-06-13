bd2dbff230dffccc97cefd6130464e1f6900cb49
Lane Seppala <lane.seppala@joyent.com>
2018-06-13 17:57:40 -0600

    Make menu bar burger work

--- src/layouts/Main/Navbar.js
+++ src/layouts/Main/Navbar.js
@@ -1,43 +1,60 @@
 import m from "mithril";
 
-const asWorkspaceLink = w =>
-	m(
-		"a.navbar-item",
-		{
-			onclick() {
-				const route = m.route.get();
-				const nonWorkspaceRoute = route.substring(
-					route.indexOf("/", 1)
-				);
-				m.route.set(`/${w.id}${nonWorkspaceRoute}`);
-			}
-		},
-		w.name
-	);
+export default () => {
+	let menuActive = false;
 
-export default {
-	view: ({ attrs: { currentWorkspace, workspaces } }) => {
-		return m(
-			".navbar[role=navigation]",
-			m(
-				".navbar-brand",
-				m(".navbar-item", m("a.title", "Conch")),
-				m(".navbar-burger", m("span"), m("span"), m("span"))
-			),
-			m(
-				".navbar-menu",
+	const asWorkspaceLink = w =>
+		m(
+			"a.navbar-item",
+			{
+				onclick() {
+					menuActive = false;
+					const route = m.route.get();
+					const nonWorkspaceRoute = route.substring(
+						route.indexOf("/", 1)
+					);
+					m.route.set(`/${w.id}${nonWorkspaceRoute}`);
+				}
+			},
+			w.name
+		);
+
+	return {
+		view: ({ attrs: { currentWorkspace, workspaces } }) => {
+			return m(
+				".navbar[role=navigation]",
 				m(
-					".navbar-end",
+					".navbar-brand",
+					m(".navbar-item", m("a.title", "Conch")),
 					m(
-						".navbar-item.has-dropdown.is-hoverable",
-						m("a.navbar-link", currentWorkspace().name),
+						".navbar-burger",
+						{
+							onclick() {
+								menuActive = !menuActive;
+							},
+							class: menuActive && "is-active"
+						},
+						m("span"),
+						m("span"),
+						m("span")
+					)
+				),
+				m(
+					".navbar-menu",
+					{ class: menuActive && "is-active" },
+					m(
+						".navbar-end",
 						m(
-							".navbar-dropdown.is-right",
-							workspaces().map(asWorkspaceLink)
+							".navbar-item.has-dropdown.is-hoverable",
+							m("a.navbar-link", currentWorkspace().name),
+							m(
+								".navbar-dropdown.is-right",
+								workspaces().map(asWorkspaceLink)
+							)
 						)
 					)
 				)
-			)
-		);
-	}
+			);
+		}
+	};
 };
