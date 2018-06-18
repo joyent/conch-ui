ea8afd658e6688045b6b7f7342692b9688444e79
Lane Seppala <lane.seppala@joyent.com>
2018-06-18 11:13:51 -0600

    feat: display workspace drop as indented tree

--- src/layouts/Main/Navbar.js
+++ src/layouts/Main/Navbar.js
@@ -1,26 +1,75 @@
 import m from "mithril";
+import keyBy from "lodash/keyBy";
+import sortBy from "lodash/sortBy";
 
 export default () => {
 	let menuActive = false;
 
-	const asWorkspaceLink = w =>
-		m(
-			"a.navbar-item",
-			{
-				onclick() {
-					menuActive = false;
-					const route = m.route.get();
-					const nonWorkspaceRoute = route.substring(
-						route.indexOf("/", 1)
-					);
-					m.route.set(`/${w.id}${nonWorkspaceRoute}`);
-				}
-			},
-			w.name
+	const WorkspaceLink = {
+		view: ({ attrs: { workspace }, children }) =>
+			m(
+				"li",
+				m(
+					"a.navbar-item",
+					{
+						onclick() {
+							menuActive = false;
+							const route = m.route.get();
+							const nonWorkspaceRoute = route.substring(
+								route.indexOf("/", 1)
+							);
+							m.route.set(`/${workspace.id}${nonWorkspaceRoute}`);
+						}
+					},
+					workspace.name
+				),
+				children
+			)
+	};
+
+	const workspaceGraphLinks = workspaceGraph => {
+		const asLink = workspace => {
+			return m(
+				WorkspaceLink,
+				{ workspace },
+				workspaceGraph.graph[workspace.id] &&
+					m(
+						"ul",
+						sortBy(workspaceGraph.graph[workspace.id], [
+							"name"
+						]).map(asLink)
+					)
+			);
+		};
+		return sortBy(workspaceGraph.roots, ["name"]).map(root =>
+			m("ul", asLink(root))
 		);
+	};
 
 	return {
 		view: ({ attrs: { currentWorkspace, workspaces } }) => {
+			const workspaceIdToWorkspace = keyBy(workspaces(), "id");
+
+			const workspaceGraph = workspaces().reduce(
+				(acc, workspace) => {
+					// global workspace
+					if (!workspace.parent_id) {
+						acc.roots.push(workspace);
+						return acc;
+					}
+
+					if (acc.graph[workspace.parent_id])
+						acc.graph[workspace.parent_id].push(workspace);
+					else acc.graph[workspace.parent_id] = [workspace];
+
+					// if the parent isn't present, then the workspace is a root
+					if (!workspaceIdToWorkspace[workspace.parent_id])
+						acc.roots.push(workspace);
+					return acc;
+				},
+				{ roots: [], graph: {} }
+			);
+
 			return m(
 				".navbar[role=navigation]",
 				m(
@@ -49,7 +98,7 @@ export default () => {
 							m("a.navbar-link", currentWorkspace().name),
 							m(
 								".navbar-dropdown.is-right",
-								workspaces().map(asWorkspaceLink)
+								workspaceGraphLinks(workspaceGraph)
 							)
 						)
 					)
