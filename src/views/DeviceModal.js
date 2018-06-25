8e507c4c56f7b7b2616f08cae6525eba44729558
Lane Seppala <lane.seppala@joyent.com>
2018-06-25 12:08:13 -0600

    feat: close modal when Escape is pressed

--- src/views/DeviceModal.js
+++ src/views/DeviceModal.js
@@ -2,38 +2,52 @@ import m from "mithril";
 
 import DeviceInspector from "./DeviceInspector";
 
-export default {
-	view: ({ attrs: { activeDeviceId } }) =>
-		m(
-			".modal.is-active",
-			// override. keep the modal at the top, not center
-			{ style: "align-items: start" },
-			m(".modal-background", {
-				onclick() {
-					activeDeviceId(null);
-				}
-			}),
+export default () => {
+	return {
+		oninit: ({ attrs: { activeDeviceId } }) => {
+			// close the modal when Esc is pressed. Only fire once
+			document.addEventListener(
+				"keydown",
+				function(evt) {
+					if (evt.keyCode == 27) {
+						activeDeviceId(null);
+					}
+				},
+				{ once: true }
+			);
+		},
+		view: ({ attrs: { activeDeviceId } }) =>
 			m(
-				".modal-card",
-				// give a bit of space
-				{ style: "margin-top: 5vh" },
+				".modal.is-active",
+				// override. keep the modal at the top, not center
+				{ style: "align-items: start" },
+				m(".modal-background", {
+					onclick() {
+						activeDeviceId(null);
+					}
+				}),
 				m(
-					"header.modal-card-head",
+					".modal-card",
+					// give a bit of space
+					{ style: "margin-top: 5vh" },
 					m(
-						"p.modal-card-title.has-text-left",
-						`Device ${activeDeviceId()}`
+						"header.modal-card-head",
+						m(
+							"p.modal-card-title.has-text-left",
+							`Device ${activeDeviceId()}`
+						),
+						m("button.delete[aria-label=close]", {
+							onclick() {
+								activeDeviceId(null);
+							}
+						})
 					),
-					m("button.delete[aria-label=close]", {
-						onclick() {
-							activeDeviceId(null);
-						}
-					})
-				),
-				m(
-					"section.modal-card-body",
-					m(DeviceInspector, { activeDeviceId })
-				),
-				m("footer.modal-card-foot.is-right")
+					m(
+						"section.modal-card-body",
+						m(DeviceInspector, { activeDeviceId })
+					),
+					m("footer.modal-card-foot.is-right")
+				)
 			)
-		)
+	};
 };
