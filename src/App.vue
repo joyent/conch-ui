e75ff6ac48dcffbd2e67e231dec21d3b3009dbb6
Dustin Ryerson <dpryerson@gmail.com>
2018-12-20 12:20:31 -0800

    Create base component and files

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="app" v-if="this.$route.path === '/'">
+        <SignIn/>
+    </div>
+    <div id="app" v-else>
+        <Layout>
+            <router-view></router-view>
+        </Layout>
+    </div>
+</template>
+
+<script>
+import Layout from './views/Layout/Layout.vue';
+import SignIn from './views/SignIn/SignIn.vue';
+
+export default {
+    components: {
+        Layout,
+        SignIn,
+    },
+};
+</script>
+
