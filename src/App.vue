b372651ee1ae5093b54d1c1c2fd48cf88aea85b7
Dustin Ryerson <dpryerson@gmail.com>
2019-01-09 13:29:08 -0800

    Persist currentWorkspace on page reload

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
     <div id="app" v-if="this.$route.path === '/'">
         <SignIn/>
     </div>
-    <div id="app" v-else>
+    <div id="app" v-else-if="hasWorkspace">
         <router-view name="navbar"></router-view>
         <div class="section">
             <div class="columns">
@@ -20,12 +20,40 @@
 <script>
 import Layout from './views/Layout/Layout.vue';
 import SignIn from './views/SignIn/SignIn.vue';
+import isEmpty from 'lodash/isEmpty';
+import { mapActions, mapState } from 'vuex';
+import { isLoggedIn } from './api/authentication';
+import { loadAllWorkspaces } from './api/workspaces';
 
 export default {
     components: {
         Layout,
         SignIn,
     },
+    methods: {
+        ...mapActions([
+            'setCurrentWorkspace',
+            'setWorkspaces',
+        ]),
+    },
+    computed: {
+        ...mapState([
+            'currentWorkspace',
+        ]),
+        hasWorkspace() {
+            return !isEmpty(this.currentWorkspace);
+        },
+    },
+    mounted() {
+        if (!this.hasWorkspace && isLoggedIn()) {
+            loadAllWorkspaces()
+                .then(response => {
+                    this.setWorkspaces(response.data);
+                    this.setCurrentWorkspace(this.$store.getters.loadCurrentWorkspace());
+                    return Promise.resolve();
+                });
+        }
+    }
 };
 </script>
 
