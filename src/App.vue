c72849ac12b86ffb8f2903102ebfbb57e7d739d0
Dustin Ryerson <dpryerson@gmail.com>
2019-01-09 13:29:33 -0800

    Persist any relevant data on page reload

--- src/App.vue
+++ src/App.vue
@@ -21,9 +21,11 @@
 import Layout from './views/Layout/Layout.vue';
 import SignIn from './views/SignIn/SignIn.vue';
 import isEmpty from 'lodash/isEmpty';
-import { mapActions, mapState } from 'vuex';
+import { mapActions, mapState, mapGetters } from 'vuex';
 import { isLoggedIn } from './api/authentication';
-import { loadAllWorkspaces } from './api/workspaces';
+import { loadAllWorkspaces, getRackById, getAllRacks } from './api/workspaces';
+import { getDeviceSettings, getDeviceDetails, getDeviceValidations } from './views/DeviceInspector/api';
+import { getValidations } from './api/validations';
 
 export default {
     components: {
@@ -32,11 +34,22 @@ export default {
     },
     methods: {
         ...mapActions([
+            'setActiveRack',
+            'setActiveRoom',
+            'setActiveDeviceDetails',
+            'setActiveDeviceSettings',
+            'setActiveDeviceValidations',
             'setCurrentWorkspace',
+            'setRackLayout',
+            'setValidations',
             'setWorkspaces',
         ]),
     },
     computed: {
+        ...mapGetters([
+            'activeDeviceId',
+            'currentWorkspaceId',
+        ]),
         ...mapState([
             'currentWorkspace',
         ]),
@@ -45,15 +58,76 @@ export default {
         },
     },
     mounted() {
+        // Reloads the currentWorkspace if the page is reloaded/refreshed.
+        // Also re-adds any relevant data to the store based on route params.
+        // This ensures that all necessary data is available to the page on reload.
         if (!this.hasWorkspace && isLoggedIn()) {
             loadAllWorkspaces()
                 .then(response => {
                     this.setWorkspaces(response.data);
                     this.setCurrentWorkspace(this.$store.getters.loadCurrentWorkspace());
-                    return Promise.resolve();
+
+                    if (this.$route.params) {
+                        if (this.$route.params.roomName) {
+                            const roomName = this.$route.params.roomName;
+                            getAllRacks(this.currentWorkspaceId)
+                                .then(response => {
+                                    let name;
+                                    let progress;
+                                    let racks;
+                                    let rooms = response.data;
+
+                                    name = Object.keys(rooms).find(room => {
+                                        return room === roomName;
+                                    });
+
+                                    racks = rooms[name];
+
+                                    if (racks.some(rack => rack["device_progress"]["FAIL"])) {
+                                        progress = "failing";
+                                    } else if (racks.some(rack => rack["device_progress"]["PASS"])) {
+                                        progress = "in progress";
+                                    } else if (racks.every(rack => rack["device_progress"]["VALID"])) {
+                                        progress = "validated";
+                                    } else {
+                                        progress = "not started";
+                                    }
+
+                                    this.setActiveRoom({ name, racks, progress });
+                                });
+
+                            if (this.$route.params.rackId) {
+                                const rackId = this.$route.params.rackId;
+                                getRackById(this.currentWorkspaceId, rackId)
+                                    .then(response => {
+                                        this.setActiveRack(response);
+                                        this.setRackLayout(response);
+                                        this.rackLoading = false;
+                                    });
+
+                                if (this.$route.params.deviceId) {
+                                    getDeviceSettings(this.activeDeviceId)
+                                        .then(response => {
+                                            this.setActiveDeviceSettings(response.data);
+                                        });
+                                    getDeviceDetails(this.activeDeviceId)
+                                        .then(response => {
+                                            this.setActiveDeviceDetails(response.data);
+                                        });
+                                    getDeviceValidations(this.activeDeviceId)
+                                        .then(response => {
+                                            this.setActiveDeviceValidations(response.data);
+                                        });
+                                    getValidations()
+                                        .then(response => {
+                                            this.setValidations(response.data);
+                                        });
+                                }
+                            }
+                        }
+                    }
                 });
         }
     }
 };
 </script>
-
