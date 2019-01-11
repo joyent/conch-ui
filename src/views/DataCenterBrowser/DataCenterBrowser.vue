78634e065f0090d5f776ad61724fc565c77b3391
Dustin Ryerson <dpryerson@gmail.com>
2019-01-10 16:17:42 -0800

    Fix device search and some code clean up

--- src/views/DataCenterBrowser/DataCenterBrowser.vue
+++ src/views/DataCenterBrowser/DataCenterBrowser.vue
@@ -8,14 +8,14 @@
                         <div class="dropdown is-right" :class="{ 'is-active': searchText }">
                             <div class="dropdown-trigger">
                                 <div class="control" :class="{ 'is-loading': !this.workspaceDevices && searchText }">
-                                    <input type="text" class="input" placeholder="Search for Device" v-model="searchText">
+                                    <input class="input" placeholder="Search for Device" v-model="searchText">
                                 </div>
                             </div>
                             <div class="dropdown-menu is-paddingless">
-                                <div class="dropdown-content" v-if="foundDevices">
+                                <div class="dropdown-content" v-if="foundDevices.length">
                                     <a class="dropdown-item" v-for="(device, index) in foundDevices" :key="index" @click="searchedDevice = device">
                                         {{ device.id }}
-                                        <span class="has-text-grey-light">{{ device.asset_tag }}</span>
+                                        <span class="has-text-grey-light" v-if="device.asset_tag">{{ device.asset_tag }}</span>
                                     </a>
                                 </div>
                             </div>
@@ -65,9 +65,8 @@ import Spinner from '../components/Spinner.vue';
 import { getRackById, getDevices, getAllRacks } from '../../api/workspaces.js';
 import { getLocation } from '../../api/device.js';
 import { mapActions, mapGetters, mapState } from 'vuex';
-import isEmpty from 'lodash/isEmpty'
-
-const maxFoundDevices = 12;
+import isEmpty from 'lodash/isEmpty';
+import search from 'fuzzysearch';
 
 export default {
     components: {
@@ -80,15 +79,15 @@ export default {
     },
     data() {
         return {
-            foundDevices: null,
+            foundDevices: [],
             highlightDeviceId: null,
+            maxFoundDevices: 12,
             rackFilterText: '',
             rackLoading: false,
             rackRooms: [],
             searchedDevice: null,
             searchText: null,
             workspaceDevices: null,
-
         };
     },
     methods: {
@@ -108,23 +107,29 @@ export default {
                 return "not started";
             }
         },
-    },
-    watch: {
-        searchedDevice(newDevice) {
-            let deviceId = newDevice.id;
-            getLocation(deviceId)
-                .then(deviceLocation => {
-                    // this.activeRoomName = deviceLocation.datacenter.name;
-                    // this.activeRackId = deviceLocation.rack.id;
-                    this.hightlightDeviceId = deviceId;
-                });
+        setFoundDevices(searchText) {
+            let devices = this.workspaceDevices;
+            let text = searchText.toLowerCase();
+
+            this.foundDevices = devices.reduce((acc, device) => {
+                if (acc.length > this.maxFoundDevices) {
+                    return acc;
+                }
+
+                let deviceId = device.id.toLowerCase();
+                let assetTag = device.asset_tag ? device.asset_tag.toLowerCase() : false;
+
+                if (search(text, deviceId) || (assetTag && search(text, assetTag))) {
+                    acc.push(device);
+                }
+
+                return acc;
+            }, []);
         },
     },
     computed: {
         ...mapGetters([
-            'activeDeviceId',
             'activeRackId',
-            // 'activeRoomName',
             'currentWorkspaceId',
         ]),
         ...mapState([
@@ -155,17 +160,24 @@ export default {
                     this.setRackLayout(response);
                     this.rackLoading = false;
                 });
-        }
+        },
+        searchText(searchText) {
+            if (searchText !== '') {
+                this.setFoundDevices(searchText);
+            }
+        },
     },
     created() {
         getDevices(this.currentWorkspaceId)
             .then(response => {
                 let devices = response.data;
 
-                this.workspaceDevices = devices.sort((a, b) => a.id = b.id);
+                devices.sort((a, b) => a.id - b.id);
+
+                this.workspaceDevices = devices;
             });
 
-        getAllRacks(this.currentWorkspace.id)
+        getAllRacks(this.currentWorkspaceId)
             .then(response => {
                 let data = response.data;
 
@@ -182,8 +194,6 @@ export default {
 
                         return acc;
                     }, []);
-
-
             });
     },
     destroyed() {
