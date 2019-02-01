b52e29e967834f9dccc88f797a2285275cc6dfad
Dustin Ryerson <dpryerson@gmail.com>
2019-02-01 14:50:18 -0800

    Remove the Mithril version of the UI

--- src/views/component/index.js
+++
@@ -1,5 +0,0 @@
-import RadialProgress from "views/component/RadialProgress";
-import Spinner from "views/component/Spinner";
-import ViewTitleHero from "views/component/ViewTitleHero";
-
-export { RadialProgress, Spinner, ViewTitleHero };
