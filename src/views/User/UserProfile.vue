499fc172e856ef8055f2a6e50097bb1fbbcfccac
Dustin Ryerson <dpryerson@gmail.com>
2019-02-08 11:23:29 -0800

    Ensure the password input does not accept empty values
    - Adds a notification if a user tries to enter a password with less than 5 characters
    - Adds some loading indicators

--- src/views/User/UserProfile.vue
+++ src/views/User/UserProfile.vue
@@ -5,15 +5,37 @@
             <div class="tile is-ancestor">
                 <div class="tile is-parent">
                     <article class="tile is-child box">
+                        <transition name="fade">
+                            <article class="message is-danger" v-if="showWarning">
+                                <div class="message-header">
+                                    <p>
+                                        <i class="fas fa-exclamation-circle" style="margin-right: 10px;"></i>
+                                        Passwords must be at least 5 characters. Please enter a secure password.
+                                    </p>
+                                    <button class="delete" aria-label="delete" @click="showWarning = false"></button>
+                                </div>
+                            </article>
+                        </transition>
                         <p class="subtitle" style="margin-bottom: 0.75rem;">New Password</p>
                         <form>
-                            <input type="password" class="input is-small" placeholder="New Password" v-model="password">
-                            <button class="button is-primary" @click="savePassword" style="margin-top: 0.75rem;">Save</button>
+                            <input
+                                type="password"
+                                class="input"
+                                :class="{ 'is-loading': isLoading }"
+                                placeholder="New Password"
+                                v-model="password"
+                            >
+                            <button
+                                class="button is-primary"
+                                :class="{ 'is-loading': isLoading }"
+                                :disabled="isLoading"
+                                @click="savePassword"
+                                style="margin-top: 0.75rem;"
+                                type="button">
+                                Save
+                            </button>
                         </form>
                     </article>
-                </div>
-                <div class="tile is-parent">
-                    <article class="tile is-child box"></article>
                 </div>
             </div>
         </section>
@@ -30,15 +52,30 @@ export default {
     },
     data() {
         return {
+            isLoading: false,
             password: '',
+            showWarning: false,
         };
     },
     methods: {
         savePassword() {
-            updatePassword(this.password)
-                .then(() => {
-                    this.$router.push({ name: 'signIn' });
-                });
+            this.isLoading = true;
+            const password = this.password;
+
+            if (password.length < 5) {
+                this.showWarning = true;
+                this.isLoading = false;
+            } else {
+                if (this.showWarning) {
+                    this.showWarning = false;
+                }
+
+                updatePassword(password)
+                    .then(() => {
+                        this.$router.push({ name: 'signIn' });
+                        this.isLoading = false;
+                    });
+            }
         },
     },
 };
