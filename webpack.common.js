9404dfe016f199ae15915ef6aee5895bfc2fde1c
Dustin Ryerson <dpryerson@gmail.com>
2019-03-15 11:07:40 -0700

    Fix the stuff I broke attempting to resolve merge conflicts during rebase

--- webpack.common.js
+++ webpack.common.js
@@ -43,6 +43,7 @@ module.exports = {
 						plugins: [
 							'@babel/plugin-syntax-dynamic-import',
 							'@babel/plugin-proposal-object-rest-spread',
+							'@babel/plugin-transform-react-jsx',
 						]
 					}
 				},
