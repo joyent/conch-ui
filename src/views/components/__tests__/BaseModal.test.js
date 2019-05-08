99c9291107e5feab2fd2fe53eeeb8255ae07a38d
Dustin Ryerson <dpryerson@gmail.com>
2019-05-08 14:55:31 -0700

    More test updates

--- src/views/components/__tests__/BaseModal.test.js
+++ src/views/components/__tests__/BaseModal.test.js
@@ -3,12 +3,20 @@ import { shallowMount } from '@vue/test-utils';
 
 describe('BaseModal.vue', () => {
     let wrapper;
+    let spy;
 
     beforeEach(() => {
         wrapper = shallowMount(BaseModal);
+        spy = jest.spyOn(wrapper.vm, 'closeModal');
     });
 
-    test('', () => {
+    test('should call the closeModal method when modal background is clicked', () => {
+        wrapper.find('.modal-background').trigger('click');
+        expect(spy).toHaveBeenCalled();
+    });
 
+    test('should call the closeModal method when the modal close button is clicked', () => {
+        wrapper.find('button.delete').trigger('click');
+        expect(spy).toHaveBeenCalled();
     });
 });
