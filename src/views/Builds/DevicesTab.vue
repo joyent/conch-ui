9869cd83781335189abdf2fab800ec194d26ca40
Dustin Ryerson <dpryerson@gmail.com>
2019-07-15 13:24:34 -0700

    Add Overview, Devices, Racks and Users tabs to the main admin Builds page

---
+++ src/views/Builds/DevicesTab.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="devices-tab">
+
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        build: {
+            type: Object,
+            required: true,
+        },
+    },
+};
+</script>
