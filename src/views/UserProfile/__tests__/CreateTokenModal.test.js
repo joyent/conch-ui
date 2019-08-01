4ef83f18ac28e9ae08df4668ff5b2286ef703e04
Dustin Ryerson <dpryerson@gmail.com>
2019-08-01 16:27:41 -0700

    Add tests for new create token modal

--- src/views/UserProfile/__tests__/CreateTokenModal.test.js
+++ src/views/UserProfile/__tests__/CreateTokenModal.test.js
@@ -2,20 +2,20 @@ import CreateTokenModal from '../CreateTokenModal.vue';
 import { shallowMount } from '@vue/test-utils';
 import * as users from '@api/users.js';
 
+jest.mock('@src/api/request.js');
+
 describe('CreateTokenModal.vue', () => {
+    let methods;
     let wrapper;
 
     beforeEach(() => {
-        wrapper = shallowMount(CreateTokenModal);
+        methods = {};
+        wrapper = shallowMount(CreateTokenModal, { methods });
     });
 
     // Helper functions
-    const clickCreateToken = () => {
-        wrapper.find('a.create-token').trigger('click');
-    };
-
     const saveNewToken = () => {
-        wrapper.find('a.create').trigger('click');
+        wrapper.find('.button.create').trigger('click');
     };
 
     const setTokenName = name => {
@@ -32,64 +32,94 @@ describe('CreateTokenModal.vue', () => {
         });
     });
 
+    test('should close the modal when the close (x) icon is clicked', () => {
+        methods = { closeModal: jest.fn() };
+        wrapper = shallowMount(CreateTokenModal, { methods });
+
+        wrapper.find('i.close').trigger('click');
+
+        expect(methods.closeModal).toHaveBeenCalled();
+    });
+
     test('should display error text if an emtpy token name is submitted', () => {
         wrapper.find('.button').trigger('click');
 
         expect(wrapper.text()).toContain('A token name is required.');
     });
 
-    // test.skip('should call createToken when valid token name is submitted', () => {
-    //     const spy = jest.spyOn(users, 'createToken');
+    test('should not call createToken if an empty token name is submitted', () => {
+        const spy = jest.spyOn(users, 'createToken');
 
-    //     clickCreateToken();
-    //     setTokenName('abcdefg');
-    //     saveNewToken();
+        saveNewToken();
 
-    //     expect(spy).toHaveBeenCalled();
-    // });
+        expect(spy).not.toHaveBeenCalled();
+    });
 
-    // test.skip('should not call createToken if an empty token name is submitted', () => {
-    //     const spy = jest.spyOn(users, 'createToken');
+    test('should call the createToken method from the API', () => {
+        const spy = jest.spyOn(users, 'createToken');
 
-    //     clickCreateToken();
-    //     setTokenName('');
-    //     saveNewToken();
+        setTokenName('Hello World');
+        saveNewToken();
 
-    //     expect(spy).not.toHaveBeenCalled();
-    // });
+        expect(spy).toHaveBeenCalled();
+    });
 
-    // test.skip('should display an error message when a duplicate token name is submitted', async () => {
-    //     jest.spyOn(users, 'createToken').mockRejectedValueOnce({
-    //         data: {
-    //             error: 'name "test 1" is already in use',
-    //         },
-    //         status: 400,
-    //     });
+    test('should display the token value created', async () => {
+        expect.assertions(1);
 
-    //     clickCreateToken();
-    //     setTokenName('test 1');
-    //     saveNewToken();
+        const token = 'dustin';
+        jest.spyOn(users, 'createToken').mockResolvedValue({
+            data: { token },
+        });
 
-    //     await new Promise(resolve =>
-    //         setTimeout(() => {
-    //             expect(
-    //                 wrapper.find('.duplicate-name-error').exists()
-    //             ).toBeTruthy();
-    //             resolve();
-    //         }, 10)
-    //     );
-    // });
+        setTokenName('apple');
+        saveNewToken();
 
-    // test.skip('should display a success message when a token is successfully created', async () => {
-    //     clickCreateToken();
-    //     setTokenName('test 1');
-    //     saveNewToken();
+        await new Promise(resolve =>
+            setTimeout(() => {
+                expect(wrapper.find('textarea').element.value).toEqual(token);
 
-    //     await new Promise(resolve =>
-    //         setTimeout(() => {
-    //             expect(wrapper.find('.message.success').exists()).toBeTruthy();
-    //             resolve();
-    //         }, 10)
-    //     );
-    // });
+                resolve();
+            }, 10)
+        );
+    });
+
+    test('should display a "Copy Token to Clipboard" button', async () => {
+        expect.assertions(1);
+
+        setTokenName('apple');
+        saveNewToken();
+
+        await new Promise(resolve =>
+            setTimeout(() => {
+                expect(wrapper.find('.copy-token').exists()).toBeTruthy();
+
+                resolve();
+            }, 10)
+        );
+    });
+
+    test('should display an error message when a duplicate token name is submitted', async () => {
+        expect.assertions(1);
+
+        jest.spyOn(users, 'createToken').mockRejectedValueOnce({
+            data: {
+                error: 'name "test 1" is already in use',
+            },
+            status: 400,
+        });
+
+        setTokenName('test 1');
+        saveNewToken();
+
+        await new Promise(resolve =>
+            setTimeout(() => {
+                expect(wrapper.text()).toContain(
+                    'You already have a token with that name.'
+                );
+
+                resolve();
+            }, 10)
+        );
+    });
 });
