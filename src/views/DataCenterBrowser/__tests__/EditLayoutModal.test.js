a43d0524d45198de9d5dbbc6bead163272500468
Dustin Ryerson <dpryerson@gmail.com>
2019-08-30 14:38:48 -0700

    Update EditLayoutModal unit tests

--- src/views/DataCenterBrowser/__tests__/EditLayoutModal.test.js
+++ src/views/DataCenterBrowser/__tests__/EditLayoutModal.test.js
@@ -1,25 +1,18 @@
 import EditLayoutModal from '../EditLayoutModal.vue';
 import Vuex from 'vuex';
 import { createLocalVue, shallowMount } from '@vue/test-utils';
-import { EventBus } from '@src/eventBus.js';
 
-// Fixture
+// Fixtures
+import devices from '@src/__fixtures__/devices.js';
 import { rack } from '@src/__fixtures__/rackLayout.js';
-
-const GlobalPlugins = {
-    install(v) {
-        v.prototype.$bus = EventBus;
-    },
-};
+import workspaces from '@src/__fixtures__/workspaces.js';
 
 const localVue = createLocalVue();
 localVue.use(Vuex);
-localVue.use(GlobalPlugins);
 
 jest.mock('../../../api/request.js');
 
 describe('EditLayoutModal.vue', () => {
-    let getters;
     let methods;
     let propsData;
     let state;
@@ -27,115 +20,158 @@ describe('EditLayoutModal.vue', () => {
     let wrapper;
 
     beforeEach(() => {
-        getters = { currentWorkspaceId: jest.fn() };
-        methods = { closeModal: jest.fn(), save: jest.fn() };
+        methods = {
+            closeModal: jest.fn(),
+            saveModifiedAssignments: jest.fn(),
+        };
         propsData = { deviceSlots: rack.slots };
-        state = { rackLayout: {} };
-        store = new Vuex.Store({ getters, state });
+        state = {
+            currentWorkspace: workspaces[0],
+            devices: devices,
+            rackLayout: {},
+        };
+        store = new Vuex.Store({ state });
         wrapper = shallowMount(EditLayoutModal, {
-            localVue, methods, propsData, store
+            localVue,
+            methods,
+            propsData,
+            store,
         });
     });
 
     // Helper function
-    const clickElement = (item) => {
+    const clickElement = item => {
         wrapper.find(item).trigger('click');
     };
 
-    test('should call closeModal method when modal background is clicked', () => {
-        clickElement('.modal-background');
-
-        expect(methods.closeModal).toHaveBeenCalled();
+    test('should not display any input fields on initial render', () => {
+        expect(wrapper.find('input').exists()).toBeFalsy();
     });
 
-    test('should call closeModal method when X button is clicked', () => {
-        clickElement('button.delete');
+    describe('closing the modal', () => {
+        test('should call closeModal method when modal background is clicked', () => {
+            clickElement('.modal-background');
 
-        expect(methods.closeModal).toHaveBeenCalled();
+            expect(methods.closeModal).toHaveBeenCalled();
+        });
+
+        test('should call closeModal method when X button is clicked', () => {
+            clickElement('button.delete');
+
+            expect(methods.closeModal).toHaveBeenCalled();
+        });
+
+        test('should call closeModal method when Close button is clicked', () => {
+            clickElement('.close-modal');
+
+            expect(methods.closeModal).toHaveBeenCalled();
+        });
     });
 
-    test('should call closeModal method when Cancel button is clicked', () => {
-        clickElement('button.cancel');
+    describe('clicking an Edit button', () => {
+        test('should display a serial number input field', () => {
+            clickElement('.edit');
 
-        expect(methods.closeModal).toHaveBeenCalled();
+            expect(wrapper.find('input.serial-number').exists()).toBeTruthy();
+        });
+
+        test('should display an asset tag input field', () => {
+            clickElement('.edit');
+
+            expect(wrapper.find('input.asset-tag').exists()).toBeTruthy();
+        });
+
+        test('should display a cancel button', () => {
+            clickElement('.edit');
+
+            expect(wrapper.find('.cancel').exists()).toBeTruthy();
+        });
+
+        test('should change the Close button to Save Changes', () => {
+            clickElement('.edit');
+
+            expect(wrapper.find('.save-changes').exists()).toBeTruthy();
+        });
     });
 
-    test('should call the save method when the save button is clicked', () => {
-        const spy = jest.spyOn(wrapper.vm, 'save');
-        clickElement('button.save');
+    describe('clicking a Cancel button', () => {
+        test('should hide the input fields', () => {
+            clickElement('.edit');
+            clickElement('.cancel');
+
+            expect(wrapper.find('input').exists()).toBeFalsy();
+        });
+
+        test('should change the Save Changes button to the Close button when last Cancel button is clicked', () => {
+            clickElement('.edit');
+            clickElement('.cancel');
+
+            expect(wrapper.find('.close-modal').exists()).toBeTruthy();
+        });
+    });
+
+    test('should call saveModifiedAssignments when Save Changes button is clicked', () => {
+        const spy = jest.spyOn(wrapper.vm, 'saveModifiedAssignments');
+
+        clickElement('.edit');
+        clickElement('.save-changes');
 
         expect(spy).toHaveBeenCalled();
     });
 
-    test('should set isActive = false on closeModal:editLayoutModal event emission', () => {
-        methods = {};
-        wrapper = shallowMount(EditLayoutModal, {
-            localVue, methods, propsData, store
+    describe('input validation', () => {
+        beforeEach(() => {
+            methods = { closeModal: jest.fn() };
+            wrapper = shallowMount(EditLayoutModal, {
+                localVue,
+                methods,
+                propsData,
+                store,
+            });
         });
-        wrapper.setData({ isActive: true });
 
-        clickElement('button.cancel');
-        expect(wrapper.vm.isActive).toEqual(false);
-    });
+        test('should show "Duplicate serial number" error', () => {
+            const deviceId = devices[1].id;
 
-    test('should set isActive = true on openModal:editLayoutModal event emission ', () => {
-        expect(wrapper.vm.isActive).toEqual(false);
-        EventBus.$emit('openModal:editLayoutModal');
-        expect(wrapper.vm.isActive).toEqual(true);
-    });
+            clickElement('.edit');
+            wrapper.find('input.serial-number').setValue(deviceId);
+            clickElement('.save-changes');
 
-    test('should display a notification if duplicate serial numbers are entered', async () => {
-        expect.assertions(1);
-        methods = {};
-        wrapper = shallowMount(EditLayoutModal, {
-            localVue, methods, propsData, store
+            expect(wrapper.html()).toContain('Duplicate serial number');
         });
-        const firstInput = wrapper.findAll('input.serial-number').at(0);
-        const secondInput = wrapper.findAll('input.serial-number').at(1);
 
-        secondInput.setValue(firstInput.element.value)
+        test('should show "Duplicate asset tag" error', () => {
+            const deviceAssetTag = devices[1].asset_tag;
 
-        await new Promise(resolve => setTimeout(() => {
-            clickElement('button.save');
-            expect(wrapper.html()).toContain('Device serial is duplicated')
-            resolve();
-        }, 800));
-    });
+            clickElement('.edit');
+            wrapper.find('input.asset-tag').setValue(deviceAssetTag);
+            clickElement('.save-changes');
 
-    test('should update slot serial number if no duplicate entries exist', async () => {
-        expect.assertions(2);
-        methods = {};
-        wrapper = shallowMount(EditLayoutModal, {
-            localVue, methods, propsData, store
+            expect(wrapper.html()).toContain('Duplicate asset tag');
         });
-        const firstInput = wrapper.findAll('input.serial-number').at(0);
-        const slot = rack.slots[0];
 
-        expect(firstInput.element.value).toEqual(slot.occupant.id);
-        firstInput.setValue('ABCDEFG');
+        test('should show "Invalid Serial Number" error when a serial number with "." is submitted', () => {
+            clickElement('.edit');
+            wrapper.find('input.serial-number').setValue('hello.world');
+            clickElement('.save-changes');
 
-        await new Promise(resolve => setTimeout(() => {
-            clickElement('button.save');
-            expect(wrapper.vm.assignments[slot.id].id).toEqual('ABCDEFG');
-            resolve();
-        }, 800));
-    });
-
-    test('should update slot asset tag', async () => {
-        expect.assertions(1);
-        methods = {};
-        wrapper = shallowMount(EditLayoutModal, {
-            localVue, methods, propsData, store
+            expect(wrapper.html()).toContain('Invalid serial number');
         });
-        const firstInput = wrapper.findAll('input.asset-tag').at(0);
-        const slot = rack.slots[0];
 
-        firstInput.setValue('ABCDEFG');
+        test('should show "Invalid Serial Number" error when a serial number with "/" is submitted', () => {
+            clickElement('.edit');
+            wrapper.find('input.serial-number').setValue('hello/world');
+            clickElement('.save-changes');
 
-        await new Promise(resolve => setTimeout(() => {
-            clickElement('button.save');
-            expect(wrapper.vm.assignments[slot.id].assetTag).toEqual('ABCDEFG');
-            resolve();
-        }, 800));
+            expect(wrapper.html()).toContain('Invalid serial number');
+        });
+
+        test('should show "Invalid Serial Number" error when a serial number with whitespace is submitted', () => {
+            clickElement('.edit');
+            wrapper.find('input.serial-number').setValue('hello world');
+            clickElement('.save-changes');
+
+            expect(wrapper.html()).toContain('Invalid serial number');
+        });
     });
 });
