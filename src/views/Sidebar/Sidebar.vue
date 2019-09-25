ca671b96a627689636b111aa7581972b27266812
Dustin Ryerson <dpryerson@gmail.com>
2019-09-25 16:17:10 -0700

    Revert "Load the currentUser on Sidebar component creation rather than from the App component"
    This reverts commit 4314b4dde98a9fdddc5c176cac24ed7630ee9e5c.

--- src/views/Sidebar/Sidebar.vue
+++ src/views/Sidebar/Sidebar.vue
@@ -1,111 +1,112 @@
 <template>
-    <aside
-        class="menu"
-        :class="{ loading: !this.workspaceId || isEmpty(this.currentUser) }"
-    >
-        <Spinner v-if="!this.workspaceId || isEmpty(this.currentUser)" />
-        <div class="sidebar" v-else>
-            <div class="brand">
-                <img
-                    class="brand-icon"
-                    src="../../assets/brand.png"
-                    width="150px"
-                    @click="navigateHomepage()"
-                />
-            </div>
-            <p class="menu-label">Datacenter Builds</p>
-            <ul class="menu-list">
-                <li class="nav-item">
-                    <router-link
-                        :to="{
-                            name: 'status',
-                            params: { currentWorkspace: this.workspaceId },
-                        }"
-                        active-class="is-active"
-                    >
-                        <i class="fas fa-lg fa-satellite-dish"></i>
-                        <span>Status</span>
-                    </router-link>
-                </li>
-                <li class="nav-item">
-                    <router-link
-                        :to="{
-                            name: 'datacenter',
-                            params: { currentWorkspace: this.workspaceId },
-                        }"
-                        active-class="is-active"
-                    >
-                        <i class="fas fa-lg fa-search"></i>
-                        <span>Browse</span>
-                    </router-link>
-                </li>
-                <li class="nav-item">
-                    <router-link
-                        :to="{
-                            name: 'devices',
-                            params: { currentWorkspace: this.workspaceId },
-                        }"
-                        active-class="is-active"
-                    >
-                        <i class="fas fa-lg fa-server"></i>
-                        <span>Devices</span>
-                    </router-link>
-                </li>
-            </ul>
-            <p v-if="currentUser.is_admin" class="menu-label">Conch Admin</p>
-            <ul class="menu-list" v-if="currentUser.is_admin">
-                <li class="nav-item">
-                    <router-link
-                        :to="{ name: 'tokens' }"
-                        active-class="is-active"
-                    >
-                        <i class="fas fa-lg fa-key"></i>
-                        <span>Tokens</span>
-                    </router-link>
-                </li>
-                <li class="nav-item">
-                    <router-link
-                        :to="{ name: 'user-management' }"
-                        active-class="is-active"
-                    >
-                        <i class="fas fa-lg fa-users"></i>
-                        <span>Users</span>
-                    </router-link>
-                </li>
-            </ul>
-            <p class="menu-label">Conch</p>
-            <ul class="menu-list">
-                <li class="nav-item">
-                    <router-link
-                        :to="{ name: 'user' }"
-                        active-class="is-active"
-                    >
-                        <i class="fas fa-lg fa-user"></i>
-                        <span>Profile</span>
-                    </router-link>
-                </li>
-                <li class="nav-item">
-                    <a class="sign-out" @click="signOut()">
-                        <i class="fas fa-lg fa-sign-out-alt"></i>
-                        <span>Log Out</span>
-                    </a>
-                </li>
-            </ul>
-            <!-- <br />
-            <div class="box conch-versions">
-                <p class="heading">Conch Versions</p>
-                <div class="tags-container">
-                    <div class="tags has-addons">
-                        <div class="tag is-primary">API</div>
-                        <div class="tag is-dark">{{ conchVersion }}</div>
-                    </div>
-                    <div class="tags has-addons">
-                        <div class="tag is-primary">UI</div>
-                        <div class="tag is-dark">{{ conchUIVersion }}</div>
-                    </div>
-                </div>
-            </div> -->
+    <Spinner v-if="!workspaceId" />
+    <aside class="menu" v-else>
+        <div class="brand">
+            <img
+                class="brand-icon"
+                src="../../assets/brand.png"
+                width="150px"
+                @click="navigateHomepage()"
+            />
         </div>
+        <p class="menu-label">Datacenter Builds</p>
+        <ul class="menu-list">
+            <li class="nav-item">
+                <router-link
+                    :to="{
+                        name: 'status',
+                        params: { currentWorkspace: this.workspaceId },
+                    }"
+                    active-class="is-active"
+                    data-tooltip="Status"
+                >
+                    <i class="fas fa-lg fa-satellite-dish"></i>
+                    <span>Status</span>
+                </router-link>
+            </li>
+            <li class="nav-item">
+                <router-link
+                    :to="{
+                        name: 'datacenter',
+                        params: { currentWorkspace: this.workspaceId },
+                    }"
+                    active-class="is-active"
+                    data-tooltip="Browse"
+                >
+                    <i class="fas fa-lg fa-search"></i>
+                    <span>Browse</span>
+                </router-link>
+            </li>
+            <li class="nav-item">
+                <router-link
+                    :to="{
+                        name: 'devices',
+                        params: { currentWorkspace: this.workspaceId },
+                    }"
+                    active-class="is-active"
+                    data-tooltip="Devices"
+                >
+                    <i class="fas fa-lg fa-server"></i>
+                    <span>Devices</span>
+                </router-link>
+            </li>
+        </ul>
+        <p v-if="currentUser.is_admin" class="menu-label">Conch Admin</p>
+        <ul class="menu-list" v-if="currentUser.is_admin">
+            <li class="nav-item">
+                <router-link
+                    :to="{ name: 'tokens' }"
+                    active-class="is-active"
+                    data-tooltip="Tokens"
+                >
+                    <i class="fas fa-lg fa-key"></i>
+                    <span>Tokens</span>
+                </router-link>
+            </li>
+            <li class="nav-item">
+                <router-link
+                    :to="{ name: 'user-management' }"
+                    active-class="is-active"
+                    data-tooltip="Users"
+                >
+                    <i class="fas fa-lg fa-users"></i>
+                    <span>Users</span>
+                </router-link>
+            </li>
+        </ul>
+        <p class="menu-label">Conch</p>
+        <ul class="menu-list">
+            <li class="nav-item">
+                <router-link
+                    :to="{ name: 'user' }"
+                    active-class="is-active"
+                    data-tooltip="Profile"
+                >
+                    <i class="fas fa-lg fa-user"></i>
+                    <span>Profile</span>
+                </router-link>
+            </li>
+            <li class="nav-item">
+                <a class="sign-out" @click="signOut()" data-tooltip="Log Out">
+                    <i class="fas fa-lg fa-sign-out-alt"></i>
+                    <span>Log Out</span>
+                </a>
+            </li>
+        </ul>
+        <!-- <br />
+        <div class="box conch-versions">
+            <p class="heading">Conch Versions</p>
+            <div class="tags-container">
+                <div class="tags has-addons">
+                    <div class="tag is-primary">API</div>
+                    <div class="tag is-dark">{{ conchVersion }}</div>
+                </div>
+                <div class="tags has-addons">
+                    <div class="tag is-primary">UI</div>
+                    <div class="tag is-dark">{{ conchUIVersion }}</div>
+                </div>
+            </div>
+        </div> -->
     </aside>
 </template>
 
@@ -114,8 +115,7 @@ import isEmpty from 'lodash/isEmpty';
 import Spinner from '@src/views/components/Spinner.vue';
 import { getApiVersion } from '@api/conchApiVersion.js';
 import { logout } from '@api/authentication.js';
-import { getCurrentUser } from '@api/users.js';
-import { mapActions, mapGetters, mapState } from 'vuex';
+import { mapGetters, mapState } from 'vuex';
 
 export default {
     components: {
@@ -128,8 +128,6 @@ export default {
         };
     },
     methods: {
-        ...mapActions(['setCurrentUser']),
-        isEmpty,
         navigateHomepage() {
             this.$router.push({
                 name: 'status',
@@ -172,12 +170,6 @@ export default {
         getApiVersion().then(response => {
             this.conchVersion = response.data.version;
         });
-
-        if (isEmpty(this.currentUser)) {
-            getCurrentUser().then(response => {
-                this.setCurrentUser(response.data);
-            });
-        }
     },
 };
 </script>
