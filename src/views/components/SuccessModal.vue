dfae052b0deff6c4d476ef5aa32e7190af0d6f30
Dustin Ryerson <dpryerson@gmail.com>
2019-10-21 17:22:08 -0700

    WIP

---
+++ src/views/components/SuccessModal.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="success-modal">
+        <div class="modal" :class="{ 'is-active': isActive }">
+            <div class="modal-background" @click="closeModal()"></div>
+            <div class="modal-card">
+                <header class="modal-card-head">
+                    <p class="modal-card-title">Success!</p>
+                    <i class="material-icons close" @click="closeModal()">
+                        close
+                    </i>
+                </header>
+                <section class="modal-card-body">
+                    <p>{{ name }} has been {{ action }}d.</p>
+                    <br />
+                    <a class="button is-success" @click="closeModal()">
+                        Close
+                    </a>
+                </section>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        action: {
+            type: String,
+            required: true,
+        },
+        name: {
+            type: String,
+            required: true,
+        },
+    },
+    data() {
+        return {
+            isActive: true,
+        };
+    },
+    methods: {
+        closeModal() {
+            this.isActive = false;
+        },
+    },
+};
+</script>
