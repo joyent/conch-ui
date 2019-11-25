f0e7e6d5e79317ea857d6ebf4ac31d8a4bb18cc0
Dustin Ryerson <dpryerson@gmail.com>
2019-11-25 08:40:59 -0800

    Resolve test errors

--- jestSetup.js
+++ jestSetup.js
@@ -2,3 +2,14 @@ import Vue from 'vue';
 
 Vue.config.devtools = false;
 Vue.config.productionTip = false;
+
+// temporary workaround for an issue with v-calendar
+window.matchMedia =
+    window.matchMedia ||
+    function() {
+        return {
+            matches: false,
+            addListener: function() {},
+            removeListener: function() {},
+        };
+    };
