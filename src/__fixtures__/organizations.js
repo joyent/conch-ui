38c1eab540cb89e4fde8b22601707333f24870b4
Dustin Ryerson <dpryerson@gmail.com>
2019-11-25 08:41:26 -0800

    Unit tests for Organizations component

---
+++ src/__fixtures__/organizations.js
@@ -0,0 +1,60 @@
+export const organizations = [
+    {
+        builds: [],
+        created: '2019-11-11T21:21:15.513286Z',
+        description: 'Engineers focusing on infrastructure development',
+        id: 'asdf3ba1-8jks-642f-3432-928av8gg5cd9',
+        name: 'Infrastructure Engineering',
+        users: [
+            {
+                email: 'admin.user@joyent.com',
+                id: 'asdf3ba1-8jks-642f-3432-928av8gg5ab1',
+                name: 'Admin User',
+                role: 'admin',
+            },
+            {
+                email: 'developer.one@joyent.com',
+                id: 'fdsa3ba1-8jks-642f-3432-928av8gg5ab3',
+                name: 'Developer One',
+                role: 'rw',
+            },
+            {
+                email: 'developer.two@joyent.com',
+                id: 'fdsa3ba1-8jks-642f-3432-928av8gg5ab3',
+                name: 'Developer Two',
+                role: 'ro',
+            },
+        ],
+        workspaces: [],
+    },
+    {
+        builds: [],
+        created: '2019-11-11T21:22:15.513286Z',
+        description: 'Engineers focusing on network development',
+        id: 'asdf3ba1-8jks-642f-3432-928av8gg5cd4',
+        name: 'Network Engineering',
+        users: [
+            {
+                email: 'admin.user@joyent.com',
+                id: 'asdf3ba1-8jks-642f-3432-928av8gg5uv9',
+                name: 'Admin User',
+                role: 'admin',
+            },
+            {
+                email: 'developer.one@joyent.com',
+                id: 'fdsa3ba1-8jks-642f-3432-928av8gg5as1',
+                name: 'Developer One',
+                role: 'rw',
+            },
+            {
+                email: 'developer.two@joyent.com',
+                id: 'fdsa3ba1-8jks-642f-3432-928av8gg5vs7',
+                name: 'Developer Two',
+                role: 'ro',
+            },
+        ],
+        workspaces: [],
+    },
+];
+
+export default organizations;
