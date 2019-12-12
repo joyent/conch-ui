d67f600f99981eeef53a0485748440df1bffcd05
Dustin Ryerson <dpryerson@gmail.com>
2019-12-12 12:30:21 -0800

    Simplify the DataCenterBrowser component

--- src/views/DataCenterBrowser/DataCenterBrowser.vue
+++ src/views/DataCenterBrowser/DataCenterBrowser.vue
@@ -1,61 +1,7 @@
 <template>
     <div class="data-center-browser">
-        <PageHeader
-            :title="`${currentWorkspaceName} workspace datacenters`"
-            :subtitle="'Browse datacenter rooms, racks and devices'"
-        />
+        <PageHeader title="Datacenter Browser" />
         <section class="info-tiles">
-            <div class="tile is-ancestor has-text-right">
-                <div class="tile is-parent">
-                    <article
-                        class="tile is-child box"
-                        style="padding: 0.75rem;"
-                    >
-                        <div
-                            class="dropdown is-right"
-                            :class="{ 'is-active': searchText }"
-                        >
-                            <div class="dropdown-trigger">
-                                <div
-                                    class="control"
-                                    :class="{
-                                        'is-loading':
-                                            !this.workspaceDevices &&
-                                            searchText,
-                                    }"
-                                >
-                                    <input
-                                        class="input search"
-                                        placeholder="Search for Device"
-                                        v-model="searchText"
-                                        @focus="hideDropdown = false"
-                                    />
-                                </div>
-                            </div>
-                            <div class="dropdown-menu is-paddingless">
-                                <div
-                                    class="dropdown-content"
-                                    v-if="foundDevices.length && !hideDropdown"
-                                >
-                                    <a
-                                        class="dropdown-item"
-                                        v-for="(device, index) in foundDevices"
-                                        :key="index"
-                                        @click="setSearchedDevice(device)"
-                                    >
-                                        {{ device.id }}
-                                        <span
-                                            class="has-text-grey-light"
-                                            v-if="device.asset_tag"
-                                            >{{ device.asset_tag }}</span
-                                        >
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    </article>
-                </div>
-            </div>
             <div class="tile is-ancestor has-text-right">
                 <div class="tile is-parent">
                     <article class="tile is-child">
@@ -74,7 +20,7 @@
                                 />
                             </div>
                             <div class="column is-6">
-                                <LayoutPanel :rack-loading="rackLoading" />
+                                <LayoutPanel />
                             </div>
                         </div>
                     </article>
@@ -85,22 +31,16 @@
 </template>
 
 <script>
-import search from 'fuzzysearch';
-import DeviceModal from '@views/components/DeviceModal.vue';
+import isEmpty from 'lodash/isEmpty';
+import DeviceModal from './DeviceModal.vue';
 import LayoutPanel from './LayoutPanel.vue';
 import PageHeader from '@views/components/PageHeader.vue';
 import RackPanel from './RackPanel.vue';
 import RoomPanel from './RoomPanel.vue';
 import Spinner from '@views/components/Spinner.vue';
 import { EventBus } from '@src/eventBus.js';
-import { getRack } from '@api/racks.js';
-import { getLocation } from '@api/device.js';
-import { mapActions, mapGetters, mapState } from 'vuex';
-import {
-    getRackRooms,
-    getWorkspaceRacks,
-    getWorkspaceDevices,
-} from '@views/shared/utils.js';
+import { mapActions, mapState } from 'vuex';
+import { getRackRooms, getWorkspaceRacks } from '@views/shared/utils.js';
 
 export default {
     components: {
@@ -114,15 +54,7 @@ export default {
     data() {
         return {
             currentWorkspaceId: '',
-            foundDevices: [],
-            hideDropdown: true,
-            maxFoundDevices: 12,
-            rackFilterText: '',
-            rackLoading: false,
             rackRooms: [],
-            searchedDevice: null,
-            searchText: null,
-            workspaceDevices: null,
         };
     },
     methods: {
@@ -131,77 +63,20 @@ export default {
             'clearActiveRoomName',
             'clearRackLayout',
             'setActiveRoomName',
-            'setHighlightDeviceId',
-            'setRackLayout',
         ]),
         clearActiveData() {
             this.clearActiveDevice();
             this.clearActiveRoomName();
             this.clearRackLayout();
         },
-        setWorkspaceDevices() {
-            getWorkspaceDevices(this.currentWorkspaceId).then(response => {
-                this.workspaceDevices = response;
-            });
-        },
+        isEmpty,
         setWorkspaceRacks() {
             getWorkspaceRacks(this.currentWorkspaceId).then(response => {
                 this.rackRooms = getRackRooms(response);
             });
         },
-        setFoundDevices(searchText) {
-            const devices = this.workspaceDevices;
-            const text = searchText.toLowerCase();
-
-            this.foundDevices = devices.reduce((acc, device) => {
-                if (acc.length > this.maxFoundDevices) {
-                    return acc;
-                }
-
-                const deviceId = device.id.toLowerCase();
-                const assetTag = device.asset_tag
-                    ? device.asset_tag.toLowerCase()
-                    : false;
-
-                if (
-                    search(text, deviceId) ||
-                    (assetTag && search(text, assetTag))
-                ) {
-                    acc.push(device);
-                }
-
-                return acc;
-            }, []);
-        },
-        setSearchedDevice(device) {
-            this.hideDropdown = true;
-            this.setHighlightDeviceId(device.id);
-
-            getLocation(device.id).then(response => {
-                const location = response.data;
-
-                if (location) {
-                    if (
-                        location.datacenter_room &&
-                        location.datacenter_room.az
-                    ) {
-                        this.setActiveRoomName(location.datacenter_room.az);
-                    }
-
-                    if (location.rack && location.rack.id) {
-                        this.rackLoading = true;
-
-                        getRack(location.rack.id).then(response => {
-                            this.setRackLayout(response.data);
-                            this.rackLoading = false;
-                        });
-                    }
-                }
-            });
-        },
     },
     computed: {
-        ...mapGetters(['currentWorkspaceName']),
         ...mapState(['activeRoomName']),
         activeRacks() {
             if (this.rackRooms.length) {
@@ -226,45 +101,20 @@ export default {
             return this.rackRooms.length > 0;
         },
     },
-    watch: {
-        searchText(searchText) {
-            if (searchText !== '') {
-                this.setFoundDevices(searchText);
-            }
-        },
-    },
     created() {
-        if (
-            this.$route &&
-            this.$route.params &&
-            this.$route.params.currentWorkspace
-        ) {
+        if (this.$route.params && this.$route.params.currentWorkspace) {
             this.currentWorkspaceId = this.$route.params.currentWorkspace;
-            this.setWorkspaceDevices();
             this.setWorkspaceRacks();
         }
     },
     mounted() {
-        if (this.$route && this.$route.params) {
-            const routeParams = this.$route.params;
-
-            if (routeParams.roomName) {
-                this.setActiveRoomName(routeParams.roomName);
-            }
-
-            if (routeParams.currentWorkspace && routeParams.rackId) {
-                this.currentWorkspaceId = routeParams.currentWorkspace;
-
-                getRack(routeParams.rackId).then(response => {
-                    this.setRackLayout(response.data);
-                });
-            }
+        if (this.$route && this.$route.params && this.$route.params.roomName) {
+            this.setActiveRoomName(this.$route.params.roomName);
         }
 
         EventBus.$on('changeWorkspace:datacenter', workspaceId => {
             this.currentWorkspaceId = workspaceId;
             this.clearActiveData();
-            this.setWorkspaceDevices();
             this.setWorkspaceRacks();
         });
     },
