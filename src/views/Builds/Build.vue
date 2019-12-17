d2a9928c80afb548be2337d3a7288f0b47911c3d
Dustin Ryerson <dpryerson@gmail.com>
2019-12-17 15:56:23 -0800

    Lots of fixes and improvements to the CreateBuildModal and related components

--- src/views/Builds/Build.vue
+++ src/views/Builds/Build.vue
@@ -1,9 +1,45 @@
 <template>
     <div class="build">
+        <transition name="fade">
+            <article class="message is-success" v-if="buildUpdated">
+                <div class="message-header">
+                    <i class="material-icons">check_circle</i>
+                    <p>Build {{ action }}</p>
+                </div>
+            </article>
+        </transition>
         <div class="build-header">
             <p class="build-name title has-text-white">
                 {{ currentBuild.name }}
             </p>
+            <div class="field is-grouped">
+                <p class="control" v-if="!currentBuild.started">
+                    <a class="button is-success" @click="updateBuild('start')">
+                        Start Build
+                    </a>
+                </p>
+                <p
+                    class="control"
+                    v-if="currentBuild.started && !currentBuild.completed"
+                >
+                    <a
+                        class="button is-success"
+                        :class="{
+                            'tooltip is-tooltip-left is-tooltip-info': !isBuildCompletable,
+                        }"
+                        @click="updateBuild('complete')"
+                        :data-tooltip="
+                            `${
+                                isBuildCompletable
+                                    ? ''
+                                    : 'Cannot complete a build with unhealthy devices'
+                            }`
+                        "
+                    >
+                        Complete Build
+                    </a>
+                </p>
+            </div>
         </div>
         <p class="build-id has-text-grey">Build ID: {{ currentBuild.id }}</p>
         <div class="tabs is-toggle">
@@ -70,6 +106,8 @@ export default {
     },
     data() {
         return {
+            action: '',
+            buildUpdated: false,
             currentTab: 'OverviewTab',
             tabs: [
                 {
@@ -149,16 +187,57 @@ export default {
                 });
             }
         },
+        updateBuild(action) {
+            const buildId = this.currentBuild.id;
+            const now = new Date();
+            let data;
+
+            if (!this.isBuildCompletable) {
+                return;
+            }
+
+            if (action === 'complete') {
+                this.action = 'completed';
+                data = { completed: now };
+            } else if (action === 'start') {
+                this.action = 'started';
+                data = { started: now };
+            }
+
+            Builds.updateBuild(buildId, data).then(() => {
+                this.buildUpdated = true;
+
+                this.getBuildData(buildId);
+
+                setTimeout(() => {
+                    this.buildUpdated = false;
+                }, 3000);
+            });
+        },
     },
     computed: {
         ...mapState([
             'currentBuild',
+            'currentBuildDevices',
             'currentWorkspace',
             'devices',
             'organizations',
             'racks',
             'users',
         ]),
+        isBuildCompletable() {
+            if (this.currentBuildDevices.length) {
+                const healthyDevicesCount = this.currentBuildDevices.filter(
+                    device => device.health === 'pass'
+                ).length;
+
+                if (healthyDevicesCount === this.currentBuildDevices.length) {
+                    return true;
+                }
+            }
+
+            return false;
+        },
     },
     created() {
         let buildId = this.buildId;
