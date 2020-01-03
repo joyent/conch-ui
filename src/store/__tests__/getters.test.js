b19be638fe1c117d7e65a502acb5cde81b9f1073
Dustin Ryerson <dpryerson@gmail.com>
2020-01-03 11:14:39 -0800

    Removing more unused workspace-related code

--- src/store/__tests__/getters.test.js
+++ src/store/__tests__/getters.test.js
@@ -2,7 +2,6 @@ import getters from '../getters.js';
 
 // Fixtures
 import activeDevice from '@src/__fixtures__/activeDevice.js';
-import workspaces from '@src/__fixtures__/workspaces.js';
 
 describe('getters', () => {
     let state;
@@ -17,16 +16,6 @@ describe('getters', () => {
             const activeDeviceId = getters.activeDeviceId(state);
 
             expect(activeDeviceId).toEqual(activeDevice.id);
-        });
-    });
-
-    describe('findWorkspaceById', () => {
-        test('it returns the workspace matching the id', () => {
-            const workspaceId = workspaces[0].id;
-            state = { workspaces };
-            const workspace = getters.findWorkspaceById(state)(workspaceId);
-
-            expect(workspace).toMatchObject(workspaces[0]);
         });
     });
 });
