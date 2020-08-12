a1ae8fc337620c6a94623e3ed0d874f2bc0fa605
Dustin Ryerson <dpryerson@gmail.com>
2020-08-12 10:09:36 -0700

    Adds more error messages and permissions and removes unused navbar component

--- src/views/Navbar/Navbar.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <div class="navbar" role="navigation">
-        <div class="navbar-menu">
-            <!-- Placeholder for Breadcrumb component -->
-        </div>
-    </div>
-</template>
