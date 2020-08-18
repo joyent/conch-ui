349d8495406017bbe4b6ec0138a1a63214535d62
Dustin Ryerson <dpryerson@gmail.com>
2020-08-18 14:48:39 -0700

    Move login token to a cookie which helps resolve application initialization issues

--- src/api/request.js
+++ src/api/request.js
@@ -1,18 +1,19 @@
 import axios from 'axios';
+import Cookies from 'js-cookie';
 import router from '@src/router.js';
 import store from '@src/store/store.js';
 import { logout } from '@api/authentication.js';
 
 export const clearToken = () => {
-    return sessionStorage.removeItem('token');
+    return Cookies.remove('token');
 };
 
 export const getToken = () => {
-    return sessionStorage.getItem('token');
+    return Cookies.get('token');
 };
 
 export const setToken = token => {
-    sessionStorage.setItem('token', token);
+    Cookies.set('token', token);
 };
 
 export const request = args => {
