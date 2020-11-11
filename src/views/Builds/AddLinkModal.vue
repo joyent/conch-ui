88f6aa3e068e74eaca76763a1543608c831de9aa
Dustin Ryerson <dpryerson@gmail.com>
2020-11-10 16:22:02 -0800

    Adds loose URL validation

--- src/views/Builds/AddLinkModal.vue
+++ src/views/Builds/AddLinkModal.vue
@@ -9,44 +9,73 @@
             close
           </i>
         </div>
-        <div class="modal-body">
-          <div class="control has-icons-left">
-            <input
-              type="text"
-              class="input"
-              v-model="link"
-              placeholder="Link"
-              @keyup.enter="confirmAction()"
-              ref="link"
-            />
-            <span class="icon is-left">
-              <span class="material-icons">link</span>
-            </span>
+        <validation-observer v-slot="{ invalid }">
+          <div class="modal-body">
+            <div class="control has-icons-left">
+              <validation-provider
+                mode="eager"
+                name="Link"
+                rules="is"
+                v-slot="{ errors }"
+              >
+                <input
+                  type="text"
+                  class="input"
+                  v-model="link"
+                  placeholder="Link"
+                  @keyup.enter="invalid ? null : confirmAction()"
+                  ref="link"
+                />
+                <span class="icon is-left">
+                  <span class="material-icons">link</span>
+                </span>
+                <p
+                  class="has-text-danger"
+                  style="margin-top: 6px; font-size: 14px"
+                  >{{ errors[0] }}</p
+                >
+              </validation-provider>
+            </div>
           </div>
-        </div>
-        <div class="modal-footer">
-          <a
-            class="button cancel is-dark"
-            :class="{ 'is-loading': isLoading }"
-            @click="closeModal()"
-          >
-            Cancel
-          </a>
-          <a
-            class="button add is-success"
-            :class="{ 'is-loading': isLoading }"
-            @click="confirmAction()"
-          >
-            Add Link
-          </a>
-        </div>
+          <div class="modal-footer">
+            <a
+              class="button cancel is-dark"
+              :class="{ 'is-loading': isLoading }"
+              @click="closeModal()"
+            >
+              Cancel
+            </a>
+            <a
+              class="button add is-success"
+              :class="{ 'is-loading': isLoading }"
+              :disabled="invalid"
+              @click="invalid ? null : confirmAction()"
+            >
+              Add Link
+            </a>
+          </div>
+        </validation-observer>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import isUrl from 'is-url';
+import { extend, ValidationProvider, ValidationObserver } from 'vee-validate';
+
+extend('is', {
+  validate: value => {
+    return isUrl(value);
+  },
+  message: 'Input must be a valid URL',
+});
+
 export default {
+  components: {
+    ValidationObserver,
+    ValidationProvider,
+  },
   data() {
     return {
       isActive: true,
